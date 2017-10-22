6d31e978c13daf21346532027b5c8af570018e4b
vinicius-pretto <vini.prettodasilva@gmail.com>
2017-10-22 20:08:17 -0200

    Adding chai

--- test/index.test.js
+++ test/index.test.js
@@ -1,11 +1,10 @@
-const assert = require('assert');
+const { expect } = require('chai');
 const { sum } = require('../index');
 
 describe('Index', () => {
     describe('sum()', () => {
         it('should return the sum of 2 + 2 that equals 4', () => {
-            const result = sum(2, 2);
-            assert.deepEqual(result, 4);
+            expect(sum(2, 2)).to.be.deep.equal(4);
         });
     });
 });
