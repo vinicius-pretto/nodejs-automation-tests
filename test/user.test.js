b2fe28adf9139627b64f98a9845b5b8bbdff8917
vinicius-pretto <vini.prettodasilva@gmail.com>
2017-10-22 21:30:19 -0200

    Adding buildUser test

--- test/user.test.js
+++ test/user.test.js
@@ -1,4 +1,5 @@
 const UserService = require('../src/users/user.service');
+const { user, expectedUser } = require('./fixtures/user.json');
 
 describe('UserService', () => {
     describe('calculateAge()', () => {
@@ -7,6 +8,14 @@ describe('UserService', () => {
             const userService = new UserService(database);
             const age = userService.calculateAge(2017, '05/18/1989');
             expect(age).to.be.deep.eql(28);
+        });
+    });
+
+    describe('buildUser()', () => {
+        it('should return a user with age equals 28', () => {
+            const database = {};
+            const userService = new UserService(database);
+            expect(userService.buildUser(user)).to.be.eql(expectedUser);
         });
     });
 });
