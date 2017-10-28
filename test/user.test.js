5d1a080a42137572683e07c57e356d9db0f36b4e
vinicius-pretto <vini.prettodasilva@gmail.com>
2017-10-28 16:44:48 -0200

    Keep simple

--- test/user.test.js
+++ test/user.test.js
@@ -2,20 +2,5 @@ const UserService = require('../src/users/user.service');
 const { user, expectedUser } = require('./fixtures/user.json');
 
 describe('UserService', () => {
-    describe('calculateAge()', () => {
-        it(`should return 28 with 2017 and '05/18/1989'`, () => {
-            const database = {};
-            const userService = new UserService(database);
-            const age = userService.calculateAge(2017, '05/18/1989');
-            expect(age).to.be.deep.eql(28);
-        });
-    });
-
-    describe('buildUser()', () => {
-        it('should return a user with age equals 28', () => {
-            const database = {};
-            const userService = new UserService(database);
-            expect(userService.buildUser(user)).to.be.eql(expectedUser);
-        });
-    });
+    
 });
