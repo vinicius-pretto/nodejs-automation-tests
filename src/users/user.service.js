f012c3e6458db5560cc5e732bab4adb296ef67ea
vinicius-pretto <vini.prettodasilva@gmail.com>
2017-10-28 16:44:48 -0200

    Keep simple

--- src/users/user.service.js
+++ src/users/user.service.js
@@ -3,21 +3,10 @@ class UserService {
         this.database = database;
     }
 
-    calculateAge(currentYear, bornDate) {
-        const year = new Date(bornDate).getFullYear();
-        return currentYear - year;
-    }
-
-    buildUser(user) {
-        const currentYear = new Date().getFullYear();
-        const age = this.calculateAge(currentYear, user.bornDate);
-        return Object.assign({}, user, { age });
-    }
-
     async findAll() {
         try {
             const users = await this.database.getAllUsers();
-            return users.map(user => this.buildUser(user));
+            return users;
         } catch (error) {
             console.log('Error on findAll()', error);
         }
