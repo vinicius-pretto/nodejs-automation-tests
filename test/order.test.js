2a304a034e06c4d3fd8141f0fbd054911bd848c8
vinicius-pretto <vini.prettodasilva@gmail.com>
2017-11-04 18:02:46 -0200

    Refactor

--- test/order.test.js
+++ test/order.test.js
@@ -1,29 +1,22 @@
 const axios = require('axios');
 const OrderService = require('../src/orders/order.service');
 const { orders } = require('./fixtures/orders.json');
-const userId = 1;
 
 describe('OrderService', () => {
-    let orderService;
-    let getStub;
-
-    before(() => {
-        orderService = new OrderService();
-        const ordersResponse = { data: { orders: orders } }
-        getStub = sinon.stub(axios, 'get');
-        getStub
-            .withArgs(`https://api/v1/orders?userId=${userId}`)
-            .returns(Promise.resolve(ordersResponse));
-    });
-
-    after(() => {
-        getStub.restore();
-    });
-
     describe(`findByUserId(1)`, () => {
-        it.only('should return all orders', async() => {
+        it('should return all orders', async() => {
+            const userId = 1;
+            const orderService = new OrderService();
+            const ordersResponse = { data: { orders: orders } };
+            const getStub = sinon.stub(axios, 'get');
+            getStub
+                .withArgs(`https://api/v1/orders?userId=${userId}`)
+                .returns(Promise.resolve(ordersResponse));
+
             const response = await orderService.findByUserId(userId);
+
             expect(response).to.be.eql(orders);
+            getStub.restore();
         });
     });
 });
